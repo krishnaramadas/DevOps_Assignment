dacd977590cb68ffd4f9c029dc7166ab5b3f2a2f
Krishna Ramadas <38761386+krishnaramadas@users.noreply.github.com>
2022-02-20 21:32:33 -0800

    Deleted unwanted code

--- calculator.py
+++ calculator.py
@@ -14,5 +14,20 @@ def subtract(a,b):
 #defining a function to multiply numbers   
 def multiply(a,b):
         return int(a)*int(b)
-            
 
+if __name__ == '__main__':
+    input1 = input('Enter first number: ')
+    input2 = input('Enter second number: ')
+    operation = input(str("Enter the operation : "))
+    
+    if operation == "+":
+        print(add(input1,input2))
+            
+    elif operation == "-":
+        print(subtract(input1,input2))
+        
+    elif operation == "*":
+        print(multiply(input1,input2))
+        
+    else:
+        print("Invalid Operation")
